2f2984793819ba5ce8c75063ecfde29af9fbd260
Carl Suster <carl.suster@sydney.edu.au>
2026-03-19 16:41:39 +1100

    Revert test addition

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,10 +1,2 @@
-library(vdiffr)
-
-round_sf_geom <- function(x, precision) {
-  new_geom <-
-    sf::st_geometry(x) |>
-    sf::st_set_precision(precision) |>
-    sf::st_as_binary() |>
-    sf::st_as_sfc(crs = sf::st_crs(x))
-  sf::st_set_geometry(x, new_geom)
-}
+library(ggplot2)
+nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
